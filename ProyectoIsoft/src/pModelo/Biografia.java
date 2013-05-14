0ecc6f115c07c08b8e415a404827f4c40f54e5b7
francisco <francisco@10.227.214.157>
2013-05-14 12:29:24 +0200

    Actualizacion de la clase biografia y su fichero

--- ProyectoIsoft/src/pModelo/Biografia.java
+++ ProyectoIsoft/src/pModelo/Biografia.java
@@ -1,5 +1,39 @@
 package pModelo;
 
-public class Biografia {
+import java.util.Date;
 
+public class Biografia {
+	private String nombre;
+	private Date fechaNac;
+	private String lugarNac;
+	private String descripcion;
+	
+	public Biografia(String nombre, Date fechaNac, String lugarNac, String descripcion){
+		this.nombre = nombre;
+		this.fechaNac = fechaNac;
+		this.lugarNac = lugarNac;
+		if(descripcion == "") this.descripcion = "No disponible por el momento, disculpe las molestias"; else this.descripcion = descripcion;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public Date getFechaNac() {
+		return fechaNac;
+	}
+
+	public String getLugarNac() {
+		return lugarNac;
+	}
+	
+	
 }
