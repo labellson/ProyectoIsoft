d25885b94f05226f285f609d35a1dc710159683b
francisco <francisco@10.227.214.116>
2013-05-15 17:33:53 +0200

    Añadido compararNombre(String nombre)

--- ProyectoIsoft/src/pModelo/ListaArtista.java
+++ ProyectoIsoft/src/pModelo/ListaArtista.java
@@ -14,4 +14,12 @@ public class ListaArtista {
 		return singelton;
 	}
 	
+	public Artista compararNombre(String nombre){
+		for(int i=0; i<listaArtista.size(); i++){
+			if(nombre.equalsIgnoreCase(listaArtista.get(i).getNombre())) return listaArtista.get(i);
+		}
+		Error.setError("Error, Artista "+nombre+" no encontrado");
+		return null;
+	}
+	
 }
