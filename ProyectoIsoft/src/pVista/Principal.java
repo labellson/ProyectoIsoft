6b0dfdc5952437e05dae997263aa47400447eaef
francisco <francisco@10.227.213.123>
2013-05-24 13:29:12 +0200

    ventana

--- ProyectoIsoft/src/pVista/Principal.java
+++ ProyectoIsoft/src/pVista/Principal.java
@@ -1,26 +1,14 @@
 package pVista;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.Image;
-import java.awt.Toolkit;
-
 import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JList;
 import javax.swing.JTextPane;
-import java.awt.Component;
-import javax.swing.Box;
-import javax.swing.ImageIcon;
+import javax.swing.SpringLayout;
 
-public class Principal extends JDialog {
+public class Principal extends JFrame {
 	private JLabel lblNewLabel;
 	private JTextField textField;
 	private JButton btnBuscar;
@@ -34,6 +22,7 @@ public class Principal extends JDialog {
 	private JLabel lblCancionesDelDisco;
 	private JLabel lblBiografia;
 	private JTextPane biografia;
+	private SpringLayout springLayout;
 
 	/**
 	 * Launch the application.
@@ -43,154 +32,85 @@ public class Principal extends JDialog {
 	}
 	private void initGUI() {
 		setBounds(100, 100, 505, 400);
-		GroupLayout groupLayout = new GroupLayout(getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(10)
-							.addComponent(getLblNewLabel())
-							.addGap(10)
-							.addComponent(getTextField(), GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(getBtnBuscar(), GroupLayout.PREFERRED_SIZE, 128, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(20)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(getLblIntegrantes())
-								.addComponent(getList(), GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
-								.addComponent(getTextField_1(), GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(getLblPosicionEnEl()))
-							.addGap(10)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(getLblCancionesDelDisco(), GroupLayout.PREFERRED_SIZE, 88, GroupLayout.PREFERRED_SIZE)
-								.addComponent(getList_1(), GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
-								.addComponent(getLblDiscos(), GroupLayout.PREFERRED_SIZE, 57, GroupLayout.PREFERRED_SIZE)
-								.addComponent(getList_2(), GroupLayout.PREFERRED_SIZE, 152, GroupLayout.PREFERRED_SIZE))
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addGroup(groupLayout.createSequentialGroup()
-									.addGap(22)
-									.addComponent(getLblBiografia()))
-								.addGroup(groupLayout.createSequentialGroup()
-									.addGap(18)
-									.addComponent(getBiografia(), GroupLayout.PREFERRED_SIZE, 142, GroupLayout.PREFERRED_SIZE)))))
-					.addGap(593))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(11)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(Alignment.TRAILING, groupLayout.createParallelGroup(Alignment.LEADING)
-							.addGroup(groupLayout.createSequentialGroup()
-								.addGap(3)
-								.addComponent(getLblNewLabel()))
-							.addComponent(getTextField(), GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addComponent(getBtnBuscar()))
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(getLblIntegrantes())
-						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-							.addComponent(getLblDiscos())
-							.addComponent(getLblBiografia())))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(getList(), GroupLayout.PREFERRED_SIZE, 118, GroupLayout.PREFERRED_SIZE)
-								.addComponent(getList_1(), GroupLayout.PREFERRED_SIZE, 118, GroupLayout.PREFERRED_SIZE))
-							.addGap(19)
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(getLblPosicionEnEl())
-								.addComponent(getLblCancionesDelDisco()))
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(getTextField_1(), GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(getList_2(), GroupLayout.PREFERRED_SIZE, 118, GroupLayout.PREFERRED_SIZE)))
-						.addComponent(getBiografia()))
-					.addGap(75))
-		);
-		getContentPane().setLayout(groupLayout);
-	}
-	private JLabel getLblNewLabel() {
-		if (lblNewLabel == null) {
-			lblNewLabel = new JLabel("Artista");
-		}
-		return lblNewLabel;
-	}
-	private JTextField getTextField() {
-		if (textField == null) {
-			textField = new JTextField();
-			textField.setColumns(10);
-		}
-		return textField;
-	}
-	private JButton getBtnBuscar() {
-		if (btnBuscar == null) {
-			btnBuscar = new JButton("Buscar");
-		}
-		return btnBuscar;
-	}
-	private JLabel getLblIntegrantes() {
-		if (lblIntegrantes == null) {
-			lblIntegrantes = new JLabel("Integrantes");
-		}
-		return lblIntegrantes;
-	}
-	private JList getList() {
-		if (list == null) {
-			list = new JList();
-		}
-		return list;
-	}
-	private JLabel getLblPosicionEnEl() {
-		if (lblPosicionEnEl == null) {
-			lblPosicionEnEl = new JLabel("Posicion");
-		}
-		return lblPosicionEnEl;
-	}
-	private JTextField getTextField_1() {
-		if (textField_1 == null) {
-			textField_1 = new JTextField();
-			textField_1.setColumns(10);
-		}
-		return textField_1;
-	}
-	private JList getList_1() {
-		if (list_1 == null) {
-			list_1 = new JList();
-		}
-		return list_1;
-	}
-	private JLabel getLblDiscos() {
-		if (lblDiscos == null) {
-			lblDiscos = new JLabel("Discos");
-		}
-		return lblDiscos;
-	}
-	private JList getList_2() {
-		if (list_2 == null) {
-			list_2 = new JList();
-		}
-		return list_2;
-	}
-	private JLabel getLblCancionesDelDisco() {
-		if (lblCancionesDelDisco == null) {
-			lblCancionesDelDisco = new JLabel("Canciones");
-		}
-		return lblCancionesDelDisco;
-	}
-	private JLabel getLblBiografia() {
-		if (lblBiografia == null) {
-			lblBiografia = new JLabel("Biografia");
-		}
-		return lblBiografia;
-	}
-	private JTextPane getBiografia() {
-		if (biografia == null) {
-			biografia = new JTextPane();
-		}
-		return biografia;
+		springLayout = new SpringLayout();
+		getContentPane().setLayout(springLayout);
+		
+		lblNewLabel = new JLabel("Artista");
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel, 17, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 10, SpringLayout.WEST, getContentPane());
+		getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		springLayout.putConstraint(SpringLayout.NORTH, textField, 14, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, textField, 52, SpringLayout.WEST, getContentPane());
+		textField.setColumns(10);
+		getContentPane().add(textField);
+		
+		btnBuscar = new JButton("Buscar");
+		springLayout.putConstraint(SpringLayout.NORTH, btnBuscar, 11, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, btnBuscar, 144, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, btnBuscar, 272, SpringLayout.WEST, getContentPane());
+		getContentPane().add(btnBuscar);
+		
+		lblIntegrantes = new JLabel("Integrantes");
+		springLayout.putConstraint(SpringLayout.NORTH, lblIntegrantes, 52, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, lblIntegrantes, 20, SpringLayout.WEST, getContentPane());
+		getContentPane().add(lblIntegrantes);
+		
+		list = new JList();
+		springLayout.putConstraint(SpringLayout.NORTH, list, 72, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, list, 20, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, list, 190, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, list, 150, SpringLayout.WEST, getContentPane());
+		getContentPane().add(list);
+		
+		lblPosicionEnEl = new JLabel("Posicion");
+		springLayout.putConstraint(SpringLayout.NORTH, lblPosicionEnEl, 209, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, lblPosicionEnEl, 20, SpringLayout.WEST, getContentPane());
+		getContentPane().add(lblPosicionEnEl);
+		
+		textField_1 = new JTextField();
+		springLayout.putConstraint(SpringLayout.NORTH, textField_1, 229, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, textField_1, 20, SpringLayout.WEST, getContentPane());
+		textField_1.setColumns(10);
+		getContentPane().add(textField_1);
+		
+		lblCancionesDelDisco = new JLabel("Canciones");
+		springLayout.putConstraint(SpringLayout.NORTH, lblCancionesDelDisco, 209, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, lblCancionesDelDisco, 160, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, lblCancionesDelDisco, 248, SpringLayout.WEST, getContentPane());
+		getContentPane().add(lblCancionesDelDisco);
+		
+		list_1 = new JList();
+		springLayout.putConstraint(SpringLayout.NORTH, list_1, 72, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, list_1, 160, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, list_1, 190, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, list_1, 310, SpringLayout.WEST, getContentPane());
+		getContentPane().add(list_1);
+		
+		lblDiscos = new JLabel("Discos");
+		springLayout.putConstraint(SpringLayout.NORTH, lblDiscos, 52, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, lblDiscos, 160, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, lblDiscos, 217, SpringLayout.WEST, getContentPane());
+		getContentPane().add(lblDiscos);
+		
+		list_2 = new JList();
+		springLayout.putConstraint(SpringLayout.NORTH, list_2, 231, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, list_2, 160, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, list_2, 349, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, list_2, 312, SpringLayout.WEST, getContentPane());
+		getContentPane().add(list_2);
+		
+		lblBiografia = new JLabel("Biografia");
+		springLayout.putConstraint(SpringLayout.NORTH, lblBiografia, 52, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, lblBiografia, 334, SpringLayout.WEST, getContentPane());
+		getContentPane().add(lblBiografia);
+		
+		biografia = new JTextPane();
+		springLayout.putConstraint(SpringLayout.NORTH, biografia, 72, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, biografia, 330, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, biografia, 349, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, biografia, 472, SpringLayout.WEST, getContentPane());
+		getContentPane().add(biografia);
 	}
 }
