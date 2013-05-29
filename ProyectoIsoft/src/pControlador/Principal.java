e9a532db78fcd1289158e7cb358c4613e5d58852
Daniel Laguna <labellson@gmail.com>
2013-05-29 16:32:41 +0200

    Añadido artistActual al controlador

--- ProyectoIsoft/src/pControlador/Principal.java
+++ ProyectoIsoft/src/pControlador/Principal.java
@@ -10,6 +10,7 @@ import javax.swing.ListModel;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 
+import pModelo.Artista;
 import pModelo.Cancion;
 import pModelo.Grupo;
 import pModelo.ListaArtista;
@@ -19,6 +20,7 @@ import pModelo.Solista;
 
 public class Principal implements ActionListener, ListSelectionListener{
   pVista.Principal vista;
+  Artista artistActual;
 	public Principal(){
 		vista = new pVista.Principal();
 		new ModeloComboBox(vista.getComboBox());
@@ -27,47 +29,52 @@ public class Principal implements ActionListener, ListSelectionListener{
 		}
 		vista.getComboBox().addActionListener(this);
 		vista.getList_1().addListSelectionListener(this);
-		
+		vista.getComboBox().setSelectedIndex(0);
+		vista.getList_1().setSelectedIndex(0);
 	}
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == vista.getComboBox()){
-			
+			vista.getList_1().removeListSelectionListener(this);
 			int indice = pModelo.ListaArtista.getSingelton().compararNombre((String) vista.getComboBox().getSelectedItem());
+			artistActual = pModelo.ListaArtista.getSingelton().get(indice);
 			if(indice == -1) return;
 			DefaultListModel<String> dListModel = new DefaultListModel<String>();
-			if(pModelo.Solista.class == pModelo.ListaArtista.getSingelton().get(indice).getClass()){
-				dListModel.addElement(pModelo.ListaArtista.getSingelton().get(indice).getNombre());
+			if(pModelo.Solista.class == artistActual.getClass()){
+				dListModel.addElement(artistActual.getNombre());
 			}else{
-				for(int i=0; i<((pModelo.Grupo)pModelo.ListaArtista.getSingelton().get(indice)).getSize();i++){
-					dListModel.addElement(((pModelo.Grupo)pModelo.ListaArtista.getSingelton().get(indice)).getIntentegrantes(i).getNombre());
+				for(int i=0; i<((pModelo.Grupo)artistActual).getSize();i++){
+					dListModel.addElement(((pModelo.Grupo)artistActual).getIntentegrantes(i).getNombre());
 				}
 			}
 			vista.getList().setModel(dListModel);
 			
 			//Discografias/Albumeso
 			DefaultListModel<String> dListModelDiscos = new DefaultListModel<String>();
-			for(int i=0; i<pModelo.ListaArtista.getSingelton().get(indice).getlAlbum().size();i++){
-				dListModelDiscos.addElement(pModelo.ListaArtista.getSingelton().get(indice).getlAlbum().get(i).getNombre());
+			for(int i=0; i<artistActual.getlAlbum().size();i++){
+				dListModelDiscos.addElement(artistActual.getlAlbum().get(i).getNombre());
 			}
 			vista.getList_1().setModel(dListModelDiscos);
+			vista.getList_1().addListSelectionListener(this);
+			vista.getList_1().setSelectedIndex(0);
 			
 			//Biografia
-			vista.getBiografia().setText(pModelo.ListaArtista.getSingelton().get(indice).getBiografia().getDescripcion());
+			vista.getBiografia().setText(artistActual.getBiografia().getDescripcion());
 		}
 	}
 	
 	public void valueChanged(ListSelectionEvent e) {
-		if(e.getSource() == vista.getList_1()){
-			int indiceDisco = (int) vista.getList_1().getSelectedIndex();
-			String artistaN = (String) vista.getComboBox().getSelectedItem();
-			DefaultListModel<String> dListModel = new DefaultListModel<String>();
-			System.out.println(pModelo.ListaArtista.getSingelton().get(pModelo.ListaArtista.getSingelton().compararNombre(artistaN)).getlAlbum().get(0).getCanciones().size());
-			ArrayList<Cancion> cancionD = pModelo.ListaArtista.getSingelton().get(pModelo.ListaArtista.getSingelton().compararNombre(artistaN)).getlAlbum().get(indiceDisco).getCanciones();
-			for(Cancion cancion : cancionD){
-				dListModel.addElement(cancion.getNombre());
-			}
-			vista.getList_2().setModel(dListModel);
+		try{
+			if(e.getSource() == vista.getList_1()){
+				int indiceDisco = (int) vista.getList_1().getSelectedIndex();
+				DefaultListModel<String> dListModel = new DefaultListModel<String>();
+				ArrayList<Cancion> cancionD = artistActual.getlAlbum().get(indiceDisco).getCanciones();
+				for(Cancion cancion : cancionD){
+					dListModel.addElement(cancion.getNombre());
+				}
+				vista.getList_2().setModel(dListModel);
+			}	
+		}catch(Exception exception){
+			exception.printStackTrace();
 		}
-		
 	}
 }
