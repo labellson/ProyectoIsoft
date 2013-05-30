9e0024f8925fe8f0da67f3ddbd1adefc2c63f8f9
francisco <francisco@francisco-PC>
2013-05-30 13:06:35 +0200

    Se ha quitado la opcion de redimensionar para lanzar un posterior DLC

--- ProyectoIsoft/src/pVista/Principal.java
+++ ProyectoIsoft/src/pVista/Principal.java
@@ -39,6 +39,7 @@ public class Principal extends JFrame {
 	private JList list_3;
 	private JTextPane textPane;
 	private JTextPane textPane_1;
+	private JList list_4;
 
 	/**
 	 * Launch the application.
@@ -48,6 +49,7 @@ public class Principal extends JFrame {
 	}
 	private void initGUI() {
 		setVisible(true);
+		setResizable(false);
 		setTitle("TicketMaster");
 		setBounds(100, 100, 800, 600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -159,7 +161,7 @@ public class Principal extends JFrame {
 		
 		scrollPane_5 = new JScrollPane();
 		sl_contentPane.putConstraint(SpringLayout.NORTH, scrollPane_5, 322, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.WEST, scrollPane_5, 0, SpringLayout.WEST, scrollPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, scrollPane_5, 10, SpringLayout.WEST, contentPane);
 		sl_contentPane.putConstraint(SpringLayout.SOUTH, scrollPane_5, 491, SpringLayout.NORTH, contentPane);
 		sl_contentPane.putConstraint(SpringLayout.EAST, scrollPane_5, 154, SpringLayout.WEST, contentPane);
 		contentPane.add(scrollPane_5);
@@ -168,21 +170,49 @@ public class Principal extends JFrame {
 		scrollPane_5.setViewportView(list_3);
 		
 		JLabel lblLetra = new JLabel("Letra");
-		sl_contentPane.putConstraint(SpringLayout.NORTH, lblLetra, 6, SpringLayout.SOUTH, scrollPane_1);
-		sl_contentPane.putConstraint(SpringLayout.WEST, lblLetra, 0, SpringLayout.WEST, lblBiografia);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, lblLetra, 342, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, lblLetra, 383, SpringLayout.WEST, contentPane);
 		contentPane.add(lblLetra);
 		
 		JScrollPane scrollPane_6 = new JScrollPane();
-		sl_contentPane.putConstraint(SpringLayout.NORTH, scrollPane_6, 6, SpringLayout.SOUTH, lblLetra);
-		sl_contentPane.putConstraint(SpringLayout.WEST, scrollPane_6, 0, SpringLayout.WEST, lblBiografia);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, scrollPane_6, 0, SpringLayout.SOUTH, lblNewLabel);
-		sl_contentPane.putConstraint(SpringLayout.EAST, scrollPane_6, 0, SpringLayout.EAST, scrollPane_1);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, scrollPane_6, 362, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, scrollPane_6, 383, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, scrollPane_6, 536, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, scrollPane_6, 566, SpringLayout.WEST, contentPane);
 		contentPane.add(scrollPane_6);
 		
 		textPane = new JTextPane();
 		textPane.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		textPane.setEditable(false);
 		scrollPane_6.setViewportView(textPane);
+		
+		JScrollPane scrollPane_7 = new JScrollPane();
+		sl_contentPane.putConstraint(SpringLayout.NORTH, scrollPane_7, 362, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, scrollPane_7, 588, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, scrollPane_7, 499, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, scrollPane_7, 774, SpringLayout.WEST, contentPane);
+		contentPane.add(scrollPane_7);
+		
+		list_4 = new JList();
+		scrollPane_7.setViewportView(list_4);
+		
+		JLabel lblCancionesPopulares = new JLabel("Canciones Populares");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, lblCancionesPopulares, 342, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, lblCancionesPopulares, 588, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, lblCancionesPopulares, 732, SpringLayout.WEST, contentPane);
+		contentPane.add(lblCancionesPopulares);
+		
+		JButton btnSalir = new JButton("Salir");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnSalir, 518, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnSalir, 685, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnSalir, 774, SpringLayout.WEST, contentPane);
+		contentPane.add(btnSalir);
+		
+		JButton btnVotar = new JButton("Votar");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnVotar, 518, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnVotar, 588, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnVotar, 677, SpringLayout.WEST, contentPane);
+		contentPane.add(btnVotar);
 		
 
 		
